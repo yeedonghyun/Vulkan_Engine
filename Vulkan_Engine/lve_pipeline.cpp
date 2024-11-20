66a9f92fa48f693d55fda335a2c0cfac4cbcba4f
DongHyun Lee <dledgus@naver.com>
2024-11-20 18:14:56 +0900

    Pipeline 구현(실행안되는게 정상)

--- Vulkan_Engine/lve_pipeline.cpp
+++ Vulkan_Engine/lve_pipeline.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <iostream>
 #include <stdexcept>
+#include <cassert>
 
 namespace lve {
 
@@ -10,10 +11,15 @@ namespace lve {
     LvePipeline::LvePipeline(LveDevice& device, 
         const std::string& vertFilePath, 
         const std::string& fragFilePath,
-        const PipeLindeConfigInfo configInfo) : lveDevice(device) {
+        const PipelineConfigInfo configInfo) : lveDevice(device) {
         createGraphicsPipeline(vertFilePath, fragFilePath, configInfo);
     }
 
+    LvePipeline::~LvePipeline() {
+        vkDestroyShaderModule(lveDevice.device(), vertShaderModule, nullptr);
+        vkDestroyShaderModule(lveDevice.device(), fragShaderModule, nullptr);
+        vkDestroyPipeline(lveDevice.device(), graphicsPipeline, nullptr);
+    }
 
     //파일을 읽고 성공했다면 vector<char> 파일 정보를 리턴
     //실패했다면 throw 
@@ -30,12 +36,83 @@ namespace lve {
         return buffer;
     }
 
-    //shader 파일 읽는 함수(파일을 읽고 테스트 하는 부분만 구현됨)
-    void LvePipeline::createGraphicsPipeline(const std::string& vertFilepath, const std::string& fragFilepath, const PipeLindeConfigInfo configInfo) {
+    //GraphicsPipeline 정의
+    void LvePipeline::createGraphicsPipeline(const std::string& vertFilepath, const std::string& fragFilepath, const PipelineConfigInfo configInfo) {
+        assert(
+            configInfo.pipelineLayout != VK_NULL_HANDLE &&
+            "Cannot create graphics pipeline: no pipelineLayout provided in configInfo");
+        assert(
+            configInfo.renderPass != VK_NULL_HANDLE &&
+            "Cannot create graphics pipeline: no renderPass provided in configInfo");
+        
         auto vertCode = readFile(vertFilepath);
         auto fragCode = readFile(fragFilepath);
-        std::cout << "Vertex Shader Code Size: " << vertCode.size() << '\n';
-        std::cout << "Fragment Shader Code Size: " << fragCode.size() << '\n';
+
+        createShaderModule(vertCode, &vertShaderModule);
+        createShaderModule(fragCode, &fragShaderModule);
+
+        //shader 
+        VkPipelineShaderStageCreateInfo shaderStages[2];
+        //구조체타입
+        shaderStages[0].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        //셰이더 종류 vertex로
+        shaderStages[0].stage = VK_SHADER_STAGE_VERTEX_BIT;
+        //생성한 모듈
+        shaderStages[0].module = vertShaderModule;
+        //진입점
+        shaderStages[0].pName = "main";
+        shaderStages[0].flags = 0;
+        shaderStages[0].pNext = nullptr;
+        shaderStages[0].pSpecializationInfo = nullptr;
+
+        shaderStages[1].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        shaderStages[1].stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+        shaderStages[1].module = fragShaderModule;
+        shaderStages[1].pName = "main";
+        shaderStages[1].flags = 0;
+        shaderStages[1].pNext = nullptr;
+        shaderStages[1].pSpecializationInfo = nullptr;
+
+        //vertex 데이터 처리방식 설정
+        VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
+        vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+        vertexInputInfo.vertexAttributeDescriptionCount = 0;
+        vertexInputInfo.vertexBindingDescriptionCount = 0;
+        vertexInputInfo.pVertexAttributeDescriptions = nullptr;
+        vertexInputInfo.pVertexBindingDescriptions = nullptr;
+
+        //그래픽 파이프라인 구성정보
+        VkGraphicsPipelineCreateInfo pipelineInfo{};
+        pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+        //셰이더 개수
+        pipelineInfo.stageCount = 2;
+        pipelineInfo.pStages = shaderStages;
+        pipelineInfo.pVertexInputState = &vertexInputInfo;
+        pipelineInfo.pInputAssemblyState = &configInfo.inputAssemblyInfo;
+        pipelineInfo.pViewportState = &configInfo.viewportInfo;
+        pipelineInfo.pRasterizationState = &configInfo.rasterizationInfo;
+        pipelineInfo.pMultisampleState = &configInfo.multisampleInfo;
+        pipelineInfo.pColorBlendState = &configInfo.colorBlendInfo;
+        pipelineInfo.pDepthStencilState = &configInfo.depthStencilInfo;
+        pipelineInfo.pDynamicState = nullptr;
+        pipelineInfo.layout = configInfo.pipelineLayout;
+        pipelineInfo.renderPass = configInfo.renderPass;
+        pipelineInfo.subpass = configInfo.subpass;
+        pipelineInfo.basePipelineIndex = -1;
+        pipelineInfo.basePipelineHandle = VK_NULL_HANDLE;
+
+        //그래픽 파이프라인 생성
+        //실패시 오류메세지 출력
+        if (vkCreateGraphicsPipelines(
+            lveDevice.device(),
+            VK_NULL_HANDLE,
+            //파이프라인 하나만 생성
+            1,
+            &pipelineInfo,
+            nullptr,
+            &graphicsPipeline) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create graphics pipeline");
+        }
     }
 
     //shader module 생성
@@ -51,11 +128,90 @@ namespace lve {
         }
     }
 
-    //초반이라 PipeLindeConfigInfo 생성하고 PipeLindeConfigInfo리턴 (나중에 안에들어갈 정보들을 추가)
-    PipeLindeConfigInfo LvePipeline::defaultPipelineConfigInfo(uint32_t width, uint32_t height)
+    // PipelineConfigInfo 생성하고 Pipeline 구성 설정
+    PipelineConfigInfo LvePipeline::defaultPipelineConfigInfo(uint32_t width, uint32_t height)
     {
-        PipeLindeConfigInfo configInfo{};
+        PipelineConfigInfo configInfo{};
+
+        //정점 설정
+        configInfo.inputAssemblyInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+        configInfo.inputAssemblyInfo.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+        configInfo.inputAssemblyInfo.primitiveRestartEnable = VK_FALSE;
+
+        //뷰포트 구성
+        configInfo.viewport.x = 0.0f;
+        configInfo.viewport.y = 0.0f;
+        configInfo.viewport.width = static_cast<float>(width);
+        configInfo.viewport.height = static_cast<float>(height);
+        configInfo.viewport.minDepth = 0.0f;
+        configInfo.viewport.maxDepth = 1.0f;
+
+        configInfo.scissor.offset = { 0, 0 };
+        configInfo.scissor.extent = { width, height };
+
+        configInfo.viewportInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+        configInfo.viewportInfo.viewportCount = 1;
+        configInfo.viewportInfo.pViewports = &configInfo.viewport;
+        configInfo.viewportInfo.scissorCount = 1;
+        configInfo.viewportInfo.pScissors = &configInfo.scissor;
+
+        //레스터 라이저
+        configInfo.rasterizationInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+        configInfo.rasterizationInfo.depthClampEnable = VK_FALSE;
+        configInfo.rasterizationInfo.rasterizerDiscardEnable = VK_FALSE;
+        configInfo.rasterizationInfo.polygonMode = VK_POLYGON_MODE_FILL;
+        configInfo.rasterizationInfo.lineWidth = 1.0f;
+        configInfo.rasterizationInfo.cullMode = VK_CULL_MODE_NONE;
+        configInfo.rasterizationInfo.frontFace = VK_FRONT_FACE_CLOCKWISE;
+        configInfo.rasterizationInfo.depthBiasEnable = VK_FALSE;
+        configInfo.rasterizationInfo.depthBiasConstantFactor = 0.0f;  
+        configInfo.rasterizationInfo.depthBiasClamp = 0.0f;           
+        configInfo.rasterizationInfo.depthBiasSlopeFactor = 0.0f;     
+
+        //멀티샘플 안티에일러싱
+        configInfo.multisampleInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+        configInfo.multisampleInfo.sampleShadingEnable = VK_FALSE;
+        configInfo.multisampleInfo.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+        configInfo.multisampleInfo.minSampleShading = 1.0f;           
+        configInfo.multisampleInfo.pSampleMask = nullptr;             
+        configInfo.multisampleInfo.alphaToCoverageEnable = VK_FALSE;  
+        configInfo.multisampleInfo.alphaToOneEnable = VK_FALSE;       
+
+        //frag 버퍼에서 색상 혼합하는 방식에 대해
+        //예를들어 색상이 겹치면 투명도 작업을 실시
+        configInfo.colorBlendAttachment.colorWriteMask =
+            VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT |
+            VK_COLOR_COMPONENT_A_BIT;
+        configInfo.colorBlendAttachment.blendEnable = VK_FALSE;
+        configInfo.colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_ONE;  
+        configInfo.colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO; 
+        configInfo.colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;             
+        configInfo.colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;  
+        configInfo.colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO; 
+        configInfo.colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;             
+
+        configInfo.colorBlendInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+        configInfo.colorBlendInfo.logicOpEnable = VK_FALSE;
+        configInfo.colorBlendInfo.logicOp = VK_LOGIC_OP_COPY; 
+        configInfo.colorBlendInfo.attachmentCount = 1;
+        configInfo.colorBlendInfo.pAttachments = &configInfo.colorBlendAttachment;
+        configInfo.colorBlendInfo.blendConstants[0] = 0.0f;  
+        configInfo.colorBlendInfo.blendConstants[1] = 0.0f;  
+        configInfo.colorBlendInfo.blendConstants[2] = 0.0f;  
+        configInfo.colorBlendInfo.blendConstants[3] = 0.0f;  
+
+        //깊이 테스트 and 스텐실 버퍼 설정
+        configInfo.depthStencilInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
+        configInfo.depthStencilInfo.depthTestEnable = VK_TRUE;
+        configInfo.depthStencilInfo.depthWriteEnable = VK_TRUE;
+        configInfo.depthStencilInfo.depthCompareOp = VK_COMPARE_OP_LESS;
+        configInfo.depthStencilInfo.depthBoundsTestEnable = VK_FALSE;
+        configInfo.depthStencilInfo.minDepthBounds = 0.0f;  
+        configInfo.depthStencilInfo.maxDepthBounds = 1.0f;  
+        configInfo.depthStencilInfo.stencilTestEnable = VK_FALSE;
+        configInfo.depthStencilInfo.front = {};  
+        configInfo.depthStencilInfo.back = {};   
+
         return configInfo;
     }
-
 }
--- Vulkan_Engine/lve_pipeline.hpp
+++ Vulkan_Engine/lve_pipeline.hpp
@@ -6,21 +6,34 @@
 
 namespace lve {
 
-	struct PipeLindeConfigInfo	{	};
+	struct PipelineConfigInfo {
+		VkViewport viewport;
+		VkRect2D scissor;
+		VkPipelineViewportStateCreateInfo viewportInfo;
+		VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+		VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+		VkPipelineMultisampleStateCreateInfo multisampleInfo;
+		VkPipelineColorBlendAttachmentState colorBlendAttachment;
+		VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+		VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+		VkPipelineLayout pipelineLayout = nullptr;
+		VkRenderPass renderPass = nullptr;
+		uint32_t subpass = 0;
+	};
 
 	class LvePipeline {
 	public:
 		//shader 경로 받아서 shader 파일 읽고 테스트 하고 디바이스 초기화
 		LvePipeline(LveDevice&device, const std::string &vertFilePath, 
-			const std::string& fragFilePath, const PipeLindeConfigInfo configInfo);
-		~LvePipeline() {}
+			const std::string& fragFilePath, const PipelineConfigInfo configInfo);
+		~LvePipeline();
 
 		//동시선언 방지
 		LvePipeline(const LvePipeline&) = delete;
 		void operator=(const LvePipeline&) = delete;
 
-		//초반이라 PipeLindeConfigInfo 생성하고 PipeLindeConfigInfo리턴 (나중에 안에들어갈 정보들을 추가)
-		static PipeLindeConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
+		//PipelineConfigInfo 생성하고 Pipeline 구성 설정
+		static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
 
 	private:
 		//파일을 읽고 성공했다면 vector<char> 파일 정보를 리턴
@@ -28,7 +41,7 @@ namespace lve {
 		static std::vector<char> readFile(const std::string& filePath);
 
 		//shader 파일 읽는 함수(파일을 읽고 테스트 하는 부분만 구현됨)
-		void createGraphicsPipeline(const std::string& vertFilePath, const std::string& fragFilePath, const PipeLindeConfigInfo configInfo);
+		void createGraphicsPipeline(const std::string& vertFilePath, const std::string& fragFilePath, const PipelineConfigInfo configInfo);
 
 		//shader module 생성
 		void createShaderModule(const std::vector<char> code, VkShaderModule* shaderModule);
