84d425a2671a67651fde2e5965866ad17228861a
DongHyun Lee <dledgus@naver.com>
2024-11-28 23:39:45 +0900

    Loading 3D Model

--- Vulkan_Engine/lve_model.hpp
+++ Vulkan_Engine/lve_model.hpp
@@ -6,22 +6,34 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm.hpp>
 
+#include <memory>
+#include <vector>
+
 namespace lve {
 	class LveModel {
 	public:
 
 		//vertex ±¸Á¶Ã¼
 		struct Vertex {
-			glm::vec3 position;
-			glm::vec3 color;
+			glm::vec3 position{};
+			glm::vec3 color{};
+			glm::vec3 normal{};
+			glm::vec2 uv{};
 
 			static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
 			static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+			
+			bool operator==(const Vertex& other) const {
+				return position == other.position && color == other.color && normal == other.normal &&
+					uv == other.uv;
+			}
 		};
 
 		struct Builder {
 			std::vector<Vertex> vertices{};
 			std::vector<uint32_t> indices{};
+
+			void loadModel(const std::string& filepath);
 		};
 
 		LveModel(LveDevice& device, const LveModel::Builder& builder);
@@ -29,6 +41,8 @@ namespace lve {
 
 		LveModel(const LveModel&) = delete;
 		LveModel& operator=(const LveModel&) = delete;
+
+		static std::unique_ptr<LveModel> createModelFromFile(LveDevice& device, const std::string& filepath);
 
 		void bind(VkCommandBuffer CommandBuffer);
 		void draw(VkCommandBuffer CommandBuffer);
