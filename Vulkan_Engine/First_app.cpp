d4b192e82ae6c85952b09669140738d30087bef9
DongHyun Lee <dledgus@naver.com>
2024-12-04 22:35:32 +0900

    Phong

--- Vulkan_Engine/First_app.cpp
+++ Vulkan_Engine/First_app.cpp
@@ -99,6 +99,7 @@ namespace lve {
 
                 ubo.projection = camera.getProjection();
                 ubo.view = camera.getView();
+                ubo.inverseView = camera.getInverseView();
                 pointLightSystem.update(frameInfo, ubo);
                 uboBuffers[frameIndex]->writeToBuffer(&ubo);
                 uboBuffers[frameIndex]->flush();
